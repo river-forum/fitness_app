c93c27e9211d8ea6755049d1da69bedf9658adba
inkusu <lnkusuin@gmail.com>
2020-10-08 00:31:33 +0900

    ci(gh action): storybookの生成物を除外

--- services/front/dev/.nuxt-storybook/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Test } from '../../components/ui/Test.vue'
-
-export const LazyTest = import('../../components/ui/Test.vue' /* webpackChunkName: "components/ui/Test" */).then(c => c.default || c)
--- services/front/dev/.nuxt-storybook/composition-api/plugin.js
+++
@@ -1,7 +0,0 @@
-
-// Necessary polyfill for Composition API support for IE11
-import 'core-js/features/reflect/own-keys'
-
-import { globalPlugin } from '@nuxtjs/composition-api'
-
-export default globalPlugin
