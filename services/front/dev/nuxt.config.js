adc7691afea0a7c1f6ad7e3a4218ee84a6330886
inkusu <lnkusuin@gmail.com>
2020-10-08 16:19:28 +0900

    fix(front): nuxt/storybook 3.0 Essential addonsの対応

--- services/front/dev/nuxt.config.js
+++ services/front/dev/nuxt.config.js
@@ -47,7 +47,5 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
-  storybook: {
-    addons: ['@storybook/addon-controls', '@storybook/addon-docs'],
-  },
+  storybook: {},
 }
