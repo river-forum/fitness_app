27d42f6b9dfc29da3d38028e61c20f717df3d924
lnkusuin <lnkusuin@gmail.com>
2020-10-16 13:57:09 +0000

    fix(front): マスターデータの追加 #7

---
+++ services/front/dev/data/master.js
@@ -0,0 +1,92 @@
+export const PART = [
+  {
+    id: 1,
+    name: 'ショルダープレス',
+    type: 'shoulder',
+  },
+  {
+    id: 2,
+    name: 'トライセプスキックバック',
+    type: 'arm',
+  },
+  {
+    id: 4,
+    name: 'チンアップ',
+    type: 'arm',
+  },
+  {
+    id: 5,
+    name: 'ダンベルカール',
+    type: 'arm',
+  },
+  {
+    id: 6,
+    name: '腕立て伏せ',
+    type: 'breast',
+  },
+  {
+    id: 7,
+    name: 'チェストプレス',
+    type: 'breast',
+  },
+  {
+    id: 8,
+    name: 'ベンチプレス',
+    type: 'breast',
+  },
+  {
+    id: 9,
+    name: 'アドミナルクランチ',
+    type: 'abdominal',
+  },
+  {
+    id: 10,
+    name: 'ラッドプルダウン',
+    type: 'back',
+  },
+  {
+    id: 11,
+    name: 'シーテッドロウ',
+    type: 'back',
+  },
+  {
+    id: 12,
+    name: 'バックエクステンション',
+    type: 'back',
+  },
+  {
+    id: 13,
+    name: 'バイシクルクランチ',
+    type: 'waist',
+  },
+  {
+    id: 14,
+    name: 'スクワット',
+    type: 'waist',
+  },
+  {
+    id: 15,
+    name: 'レッグプレス',
+    type: 'legs',
+  },
+  {
+    id: 16,
+    name: 'レッグエクステンション',
+    type: 'legs',
+  },
+  {
+    id: 17,
+    name: 'アダクター',
+    type: 'legs',
+  },
+  {
+    id: 18,
+    name: 'レッグカール',
+    type: 'legs',
+  },
+  {
+    id: 19,
+    name: 'アブダクター',
+    type: 'legs',
+  },
+]
